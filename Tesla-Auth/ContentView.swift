887fbf248a08a9ea3410d931d518b6d9b6f65bdd
Nick Sparks <sparksy64@hotmail.com>
2020-06-07 16:21:27 +0100

    Initial Commit

---
+++ Tesla-Auth/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Tesla-Auth
+//
+//  Created by Nick Sparks on 07/06/2020.
+//  Copyright © 2020 Nick Sparks. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
